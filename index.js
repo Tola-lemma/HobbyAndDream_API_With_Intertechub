1ceb677efd9f221fa39c592018b803e34d807e3d
Tola-lemma <tolalemma@gmail.com>
2024-11-15 20:05:32 +0300

    feat:dotenv is intalled and used for the purpose of PORT number

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =require('express');
-const PORT = 3000
+require('dotenv').config();
+const PORT = process.env.PORT || 3000
 const app = express();
 app.use(express.json());
 app.get('/',(req, res)=>{
