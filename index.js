e2b5acabcd5b581c0eeacd51098d6c9794aa8095
Tola-lemma <tolalemma@gmail.com>
2024-11-15 21:50:16 +0300

    Feat: Custom 404 Error Handler for invalid routes is Implemented

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ app.get('/',(req, res)=>{
 })
 //API
 app.use('/',require('./route/route'))
+
+// Custom 404 Error Handler for invalid routes
+app.use((req, res) => {
+      res.status(404).json({
+        error: "Sorry Route not found!",
+        message: `The route '${req.originalUrl}' does not exist on this server. Please check your request and try again.`
+      });
+    });
 app.listen(PORT,()=>console.log(`Server is Running and Listen on Port number ${PORT}...`))
